5ee2983f9d6dd9fb3171697e1d46d85a2bd47b26
Robert James Nielsen <robert.j.nielsen@outlook.com>
2019-12-30 14:56:58 -0800

    Initial sequence generates, does not flash yet.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Declare function to toggle between instructions and game.
 function toggleGame() {
   var gameBtn = document.getElementById('game-btn');
 
@@ -12,4 +13,47 @@ function toggleGame() {
   document.getElementById('game-info').classList.toggle('hidden');
   document.getElementById('game-display').classList.toggle('hidden');
 }
+
+// Declare arrays for cards / card sequence.
+var colorCards = [];
+var cardSequence = [];
+
+// Declare constructor for cards.
+function Card(index = 0) {
+  this.index = index;
+  colorCards.push(this);
+}
+
+// Declare render method for Card instances.
+Card.prototype.render = function() {
+  var cardContainer = document.getElementById('card-container');
+  var newCard = document.createElement('div');
+  newCard.classList.add('color-card');
+  cardContainer.appendChild(newCard);
+};
+
+// Generate initial random sequence.
+function initSequence() {
+  for (let i = 0; i < colorCards.length; i++) {
+    cardSequence.push(Math.floor(Math.random() * colorCards.length));
+  }
+  cardSequence.forEach(performSequence);
+}
+
+function performSequence(sequenceIndex) {
+  for (let i = 0; i < colorCards.length; i++) {
+    if (colorCards[i].index === sequenceIndex) {
+      console.log(`Color Card index #: ${i}`);
+    }
+  }
+}
+
+// Declare event listener for the game button.
 document.getElementById('game-btn').addEventListener('click', toggleGame);
+
+// Create and render four initial Card instances.
+for (let i = 0; i < 4; i++) {
+  new Card(i).render();
+}
+
+initSequence();
