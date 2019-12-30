93634d63caaf4ec15c768026fafd9299dee512b6
Robert James Nielsen <robert.j.nielsen@outlook.com>
2019-12-30 13:36:17 -0800

    Play Game btn now shows / hides game and instructions.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 'use strict';
 
-// Grab login button by ID.
-var loginForm = document.getElementById('login-form');
+function toggleGame() {
+  var gameBtn = document.getElementById('game-btn');
 
-loginForm.addEventListener('submit', function(e) {
-  e.preventDefault();
+  if (gameBtn.textContent === 'Play Game') {
+    gameBtn.textContent = 'Instructions';
+  } else if (gameBtn.textContent === 'Instructions') {
+    gameBtn.textContent = 'Play Game';
+  }
+
   document.getElementById('game-info').classList.toggle('hidden');
   document.getElementById('game-display').classList.toggle('hidden');
-});
+}
+document.getElementById('game-btn').addEventListener('click', toggleGame);
