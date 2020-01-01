9a1922005d1e89d740bbfd5cd4fd491fd61e99b7
Robert James Nielsen <robert.j.nielsen@outlook.com>
2019-12-31 20:02:59 -0800

    Working toward storing results.

--- js/app.js
+++ js/app.js
@@ -14,18 +14,20 @@ function toggleGame() {
   document.getElementById('game-display').classList.toggle('hidden');
 }
 
-// Declare arrays for cards, card sequence, and clicked cards.
-var colorCards = [];
-var cardSequence = [];
-var clickedCards = [];
+/*********************************/
+/* Data related to Card objects. */
+/*********************************/
 
-// Declare constructor for cards.
+// Declare variable to hold an array of Card instances.
+var colorCards = [];
+
+// Declare constructor for Card instances.
 function Card(index = 0) {
   this.index = index;
   colorCards.push(this);
 }
 
-// Declare render method for Card instances.
+// Declare render() method for Card instances.
 Card.prototype.render = function() {
   var cardContainer = document.getElementById('card-container');
   var newCard = document.createElement('div');
@@ -34,13 +36,27 @@ Card.prototype.render = function() {
   cardContainer.appendChild(newCard);
 };
 
+// Create and render four initial Card instances.
+for (let i = 0; i < 4; i++) {
+  new Card(i).render();
+}
+
+// Grab all rendered Card elements.
+var cards = document.querySelectorAll('.color-card');
+
+/*****************************************************/
+/* Data related to the game sequence and user input. */
+/*****************************************************/
+
+// Declare array to hold random Card sequence numbers.
+var cardSequence = [];
+
 // Generate initial random sequence.
 function initSequence() {
   for (let i = 0; i < colorCards.length; i++) {
     cardSequence.push(Math.floor(Math.random() * colorCards.length));
   }
   performSequence();
-  addCardClicks();
 }
 
 // Declare function that flashes Card elements in sequence.
@@ -56,47 +72,77 @@ function performSequence() {
       }, 500);
     }, 1000 * (timer + 1));
   }
+  setTimeout(addCardClicks, 1000 * (cardSequence.length + 1));
 }
 
-// Declare event listener for the game button.
+// Declare variable for counting clicks per round.
+var clickCount = 0;
+
+// Declare array to hold the IDs of clicked Card elements.
+var clickedCards = [];
+
+// Compare the clicked element to the corresponding sequence element.
+function clickCompare(event) {
+  clickedCards.push(parseInt(event.target.id));
+  let cardID = document.getElementById(event.target.id);
+  if (clickedCards[clickCount] === cardSequence[clickCount]) {
+    cardID.style.backgroundColor = 'green';
+    setTimeout(function() {
+      cardID.style.backgroundColor = 'blue';
+    }, 500);
+    clickCount++;
+  } else {
+    removeCardClicks();
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].style.backgroundColor = 'red';
+    }
+    failResult();
+  }
+  if (clickCount === cardSequence.length) {
+    roundCount++;
+    removeCardClicks();
+    setTimeout(function() {
+      for (let i = 0; i < cards.length; i++) {
+        cards[i].style.backgroundColor = 'green';
+      }
+    }, 500);
+  }
+}
+
+/******************************************/
+/* Data Related To Scoring & localStorage */
+/******************************************/
+
+// Declare variable to tally current round.
+let roundCount = 0;
+
+// Declare function to update score upon failure.
+function failResult() {
+  var failScore = document.getElementById('fail-score');
+  failScore.textContent = roundCount;
+}
+
+/***********************************************/
+/* Functions & Data Related To Event Listeners */
+/***********************************************/
+
+// Declare event listener for the game / instructions button.
 document.getElementById('game-btn').addEventListener('click', toggleGame);
 
-// Create and render four initial Card instances.
-for (let i = 0; i < 4; i++) {
-  new Card(i).render();
-}
-
-// Declare event listener for the START button.
+// Declare event listener for the START button to begin the game.
 document.getElementById('game-start').addEventListener('click', function() {
   initSequence();
   document.getElementById('game-start').classList.add('hidden');
   document.getElementById('card-container').style.paddingTop = '5.6rem';
 });
 
-// var cardClick = Array.from(document.getElementsByClassName('color-card'));
-var clickCount = 0;
-
-// Compare the clicked element to the corresponding sequence element.
-function clickCompare(element) {
-  clickedCards.push(parseInt(element.id));
-  let cardID = document.getElementById(element.id);
-  if (clickedCards[clickCount] === cardSequence[clickCount]) {
-    cardID.style.backgroundColor = 'green';
-    setTimeout(function() {
-      cardID.style.backgroundColor = 'blue';
-    }, 500);
-  } else {
-    let allCards = document.getElementsByClassName('color-card');
-    for (let i = 0; i < allCards.length; i++) {
-      allCards[i].style.backgroundColor = 'red';
-      removeCardClicks();
-    }
-  }
-  clickCount++;
-}
-
+// Declare functions for Card event handlers.
 function cursorMouse(event) {
   event.target.style.cursor = 'pointer';
+}
+
+function defaultMouse(event) {
+  event.target.style.cursor = 'default';
 }
 
 function yellowMouse(event) {
@@ -111,34 +157,37 @@ function clickMouse(event) {
   clickCompare(event);
 }
 
-// Add event listeners on each Card element.
-
-let cards = document.querySelectorAll('.color-card');
-
-cards.forEach(function(card) {
-  card.addEventListener('mouseenter', cursorMouse);
-});
-
+// Add event listeners to each Card element.
 function addCardClicks() {
-  event.srcElement.addEventListener(
-    'mouseenter',
-    cursorMouse(event.srcElement)
-  );
-  event.srcElement.addEventListener(
-    'mouseenter',
-    yellowMouse(event.srcElement)
-  );
-  event.srcElement.addEventListener('mouseleave', blueMouse(event.srcElement));
-  event.srcElement.addEventListener('click', clickMouse(event.srcElement));
+  cards.forEach(function(card) {
+    card.addEventListener('mouseenter', cursorMouse);
+  });
+  cards.forEach(function(card) {
+    card.addEventListener('mouseenter', yellowMouse);
+  });
+  cards.forEach(function(card) {
+    card.addEventListener('mouseleave', blueMouse);
+  });
+  cards.forEach(function(card) {
+    card.addEventListener('click', clickMouse);
+  });
 }
-// cardClick.forEach(addCardClicks());
 
-// Remove event listeners on each Card element.
+// Remove event listeners from each Card element.
 function removeCardClicks() {
-  cardClick.forEach(function(element) {
-    element.removeEventListener('mouseenter', cursorMouse(element));
-    element.removeEventListener('mouseenter', yellowMouse(element));
-    element.removeEventListener('mouseleave', blueMouse(element));
-    element.removeEventListener('click', clickMouse(element));
+  cards.forEach(function(card) {
+    card.removeEventListener('mouseenter', cursorMouse);
+  });
+  cards.forEach(function(card) {
+    card.addEventListener('mouseenter', defaultMouse);
+  });
+  cards.forEach(function(card) {
+    card.removeEventListener('mouseenter', yellowMouse);
+  });
+  cards.forEach(function(card) {
+    card.removeEventListener('mouseleave', blueMouse);
+  });
+  cards.forEach(function(card) {
+    card.removeEventListener('click', clickMouse);
   });
 }
